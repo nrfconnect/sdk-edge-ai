060a3d0db5849d655b0fb287bb03ee9564827136
Szymon Antkowiak <szymon.antkowiak@nordicsemi.no>
2025-08-12 16:12:19 +0200

    applications: machine_learning: nRF54LM20 config
    Add configuration for nRF54LM20 in machine learning application.
    
    Especially add configurations to profile application to compare
    ML frameworks by selecting appropriate runner.
    
    Add new ML models in form of zip archives into repo structure.
    
    Signed-off-by: Szymon Antkowiak <szymon.antkowiak@nordicsemi.no>

---
+++ applications/machine_learning/configuration/nrf54lm20dk_nrf54lm20a_cpuapp/buttons_def.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <caf/gpio_pins.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/devicetree/gpio.h>
+
+/* This configuration file is included only once from buttons module and holds
+ * information about pins forming keyboard matrix.
+ */
+
+/* This structure enforces the header file is included only once in the build.
+ * Violating this requirement triggers a multiple definition error at link time.
+ */
+const struct {} buttons_def_include_once;
+
+static const struct gpio_pin col[] = {};
+
+static const struct gpio_pin row[] = {
+	{ .port = 1, .pin = DT_GPIO_PIN(DT_NODELABEL(button0), gpios) },
+};
