9c841e78cfb3d63debc45cead9807fb01f4d484b
Roman <letvinoff@gmail.com>
2025-12-05 16:59:45 +0400

    samples: fix missing licence header, cmake indenting, some naming

--- samples/nrf_edgeai/regression/src/main.c
+++ samples/nrf_edgeai/regression/src/main.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
 /**
  * @file main.c
  * @brief Air Quality Prediction Regression Model
