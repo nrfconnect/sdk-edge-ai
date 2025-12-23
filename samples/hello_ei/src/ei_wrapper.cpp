1eff291d48bde8212b780e2eb3ab979655a43eb1
Bartosz Meus <bartosz.meus@nordicsemi.no>
2025-12-23 14:44:38 +0100

    samples: Add hello_ei Edge Impulse ML inference sample
    Add a new sample application demonstrating Edge Impulse machine learning
    integration on nRF54L15DK.
    
    The sample is based on edge_impulse/wrapper sample from NCS. However, it
    does not use ei_wrapper library from NCS, but uses a very simplified
    local wrapper for EI library. Its purpose is to separate C application
    code from C++ EI library. The management of input data is handled by
    the application, not by the wrapper anymore.
    
    Features:
    - Modular build system with separate CMakeLists for app and Edge Impulse library.
    - C/C++ wrapper interface (ei_wrapper) providing C-compatible API for ML operations.
    - Support for multiple inference runs.
    - Configurable sample buffer.
    - 2 test data sets (sine and traingle wave) for inference validation.
    
    Technical details:
    - Uses ExternalProject to download, patch and build Edge Impulse SDK.
    - Full Newlib C library for C++ standard library support (cmath, cfloat, etc.).
    
    Tested on: nRF54L15DK
    
    Signed-off-by: Bartosz Meus <bartosz.meus@nordicsemi.no>

---
+++ samples/hello_ei/src/ei_wrapper.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "ei_run_classifier.h"
+
+#if !CONFIG_ZTEST
+/* Fixes warnings about redefinition of Zephyr ROUND_UP macro. */
+#ifdef ROUND_UP
+#undef ROUND_UP
+#endif
+#endif /* !CONFIG_ZTEST */
+
+#include "ei_wrapper.h"
+
+#define DEBUG_MODE		IS_ENABLED(CONFIG_EI_WRAPPER_DEBUG_MODE)
+
+get_data_callback_t get_data_cbk = nullptr;
+
+int ei_wrapper_init(get_data_callback_t cbk)
+{
+	get_data_cbk = cbk;
+	return 0;
+}
+
+int ei_wrapper_run_inference(ei_impulse_result_t *ei_result, size_t window_size)
+{
+	signal_t features_signal;
+
+	__ASSERT(ei_result != nullptr, "ei_result pointer is null");
+
+	features_signal.get_data = get_data_cbk;
+	features_signal.total_length = window_size;
+
+	EI_IMPULSE_ERROR err = run_classifier(&features_signal, ei_result, DEBUG_MODE);
+
+	return err;
+}
