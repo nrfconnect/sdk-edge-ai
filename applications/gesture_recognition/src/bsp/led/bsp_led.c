597ea121c3a69d9a4ee8377e91a38e1085284502
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2026-01-19 15:45:27 +0100

    applications: gesture_recognition: Make LEDs more universal
    Make LEDs modules to support both RGB and
    monochromatic LEDs.
    
    Extand LED support for GPIO for
    devices that don't support PWM LEDs
    
    JIRA: NCSDK-36990
    
    Signed-off-by: Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>

--- applications/gesture_recognition/src/bsp/led/bsp_led.c
+++ applications/gesture_recognition/src/bsp/led/bsp_led.c
@@ -6,29 +6,60 @@
 
 #include "bsp_led.h"
 #include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(bsp_led, CONFIG_LOG_DEFAULT_LEVEL);
 
 
-static const struct pwm_dt_spec red_pwm_led_ = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
-static const struct pwm_dt_spec green_pwm_led_ = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led1));
-static const struct pwm_dt_spec blue_pwm_led_ = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led2));
+/*
+ * LED channels: map to RGB on boards with a tri-color LED, or to LED0..LED2
+ * on boards with discrete LEDs.
+ */
+#define HAS_PWM_LEDS (DT_NODE_HAS_STATUS(DT_ALIAS(pwm_led0), okay) && \
+		      DT_NODE_HAS_STATUS(DT_ALIAS(pwm_led1), okay) && \
+		      DT_NODE_HAS_STATUS(DT_ALIAS(pwm_led2), okay))
 
+#define HAS_GPIO_LEDS (DT_NODE_HAS_STATUS(DT_ALIAS(led0), okay) && \
+		       DT_NODE_HAS_STATUS(DT_ALIAS(led1), okay) && \
+		       DT_NODE_HAS_STATUS(DT_ALIAS(led2), okay))
+
+#if HAS_PWM_LEDS
+static const struct pwm_dt_spec led0_ = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+static const struct pwm_dt_spec led1_ = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led1));
+static const struct pwm_dt_spec led2_ = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led2));
+
+#define led_init_   init_led_pwm_
+#define led_set_    set_led_pwm_
+
+#elif HAS_GPIO_LEDS
+static const struct gpio_dt_spec led0_ = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
+static const struct gpio_dt_spec led1_ = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios, {0});
+static const struct gpio_dt_spec led2_ = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios, {0});
+
+#define led_init_   init_led_gpio_
+#define led_set_    set_led_gpio_
+
+#else
+
+#error "No LED backend available in devicetree"
+
+#endif
 
 /* not calibrated, fixed for specific board */
 #define PWM_PERIOD PWM_MSEC(20)
 
 
-static int init_led_(const struct pwm_dt_spec pwm_led)
+__unused static int init_led_pwm_(const struct pwm_dt_spec pwm_led)
 {
 	int ret = 0;
 
 	if (!device_is_ready(pwm_led.dev)) {
 		LOG_ERR("PWM LED Init error '%s' device_is_ready()", pwm_led.dev->name);
-		return ret;
+		return -ENODEV;
 	}
 
 	ret = pwm_set_dt(&pwm_led, PWM_PERIOD, 0);
@@ -39,12 +70,32 @@ static int init_led_(const struct pwm_dt_spec pwm_led)
 	return ret;
 }
 
-static int set_led_(const struct pwm_dt_spec pwm_led, float brightness)
+__unused static int init_led_gpio_(const struct gpio_dt_spec gpio_led)
 {
 	int ret = 0;
-	uint32_t pulse = brightness * PWM_PERIOD;
 
-	ret = pwm_set_pulse_dt(&pwm_led, pulse);
+	if (!device_is_ready(gpio_led.port)) {
+		LOG_ERR("GPIO LED Init error '%s' device_is_ready()", gpio_led.port->name);
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure_dt(&gpio_led, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0) {
+		LOG_ERR("Error %d: failed to configure GPIO LED on %s pin %u",
+			ret, gpio_led.port->name, gpio_led.pin);
+		return ret;
+	}
+
+	return ret;
+}
+
+__unused static int set_led_pwm_(const struct pwm_dt_spec *pwm_led, float brightness)
+{
+	int ret = 0;
+	float pwm_brightness = CLAMP(brightness, 0.0f, 1.0f);
+	uint32_t pulse = pwm_brightness * PWM_PERIOD;
+
+	ret = pwm_set_pulse_dt(pwm_led, pulse);
 	if (ret < 0) {
 		LOG_ERR("LED Init error pwm_set_pulse_dt()");
 		return ret;
@@ -52,77 +103,104 @@ static int set_led_(const struct pwm_dt_spec pwm_led, float brightness)
 	return ret;
 }
 
-int bsp_led_init(void)
+__unused static int set_led_gpio_(const struct gpio_dt_spec *gpio_led, float brightness)
 {
-	int ret;
+	int ret = 0;
+	int on = brightness > 0.0f;
 
-	ret = init_led_(red_pwm_led_);
-	BSP_RETURN_IF(ret != 0, ret);
+	ret = gpio_pin_set_dt(gpio_led, on);
+	if (ret < 0) {
+		LOG_ERR("LED Init error gpio_pin_set_dt()");
+		return ret;
+	}
 
-	ret = init_led_(green_pwm_led_);
-	BSP_RETURN_IF(ret != 0, ret);
-
-	ret = init_led_(blue_pwm_led_);
 	return ret;
 }
 
-int bsp_led_set_red(float brightness)
+int led_init(void)
 {
-	pwm_set_pulse_dt(&green_pwm_led_, 0);
-	pwm_set_pulse_dt(&blue_pwm_led_, 0);
-	return set_led_(red_pwm_led_, brightness);
+	int ret = 0;
+
+	ret = led_init_(led0_);
+	HW_RETURN_IF(ret != 0, ret);
+
+	ret = led_init_(led1_);
+	HW_RETURN_IF(ret != 0, ret);
+
+	return led_init_(led2_);
 }
 
-int bsp_led_set_green(float brightness)
+int bsp_led_set_led0(float brightness)
 {
-	pwm_set_pulse_dt(&red_pwm_led_, 0);
-	pwm_set_pulse_dt(&blue_pwm_led_, 0);
-	return set_led_(green_pwm_led_, brightness);
+	int ret = 0;
+
+	ret = led_set_(&led1_, 0);
+	HW_RETURN_IF(ret != 0, ret);
+
+	ret = led_set_(&led2_, 0);
+	HW_RETURN_IF(ret != 0, ret);
+
+	return led_set_(&led0_, brightness);
+
 }
 
-int bsp_led_set_blue(float brightness)
+int bsp_led_set_led1(float brightness)
 {
-	pwm_set_pulse_dt(&red_pwm_led_, 0);
-	pwm_set_pulse_dt(&green_pwm_led_, 0);
-	return set_led_(blue_pwm_led_, brightness);
+	int ret = 0;
+
+	ret = led_set_(&led0_, 0);
+	HW_RETURN_IF(ret != 0, ret);
+
+	ret = led_set_(&led2_, 0);
+	HW_RETURN_IF(ret != 0, ret);
+
+	return led_set_(&led1_, brightness);
 }
 
-int bsp_led_set_rgb(float r, float g, float b)
+int bsp_led_set_led2(float brightness)
 {
-	int ret;
-	uint32_t red = r * PWM_PERIOD;
-	uint32_t green = g * PWM_PERIOD;
-	uint32_t blue = b * PWM_PERIOD;
+	int ret = 0;
 
-	ret = pwm_set_pulse_dt(&red_pwm_led_, red);
-	BSP_RETURN_IF(ret != 0, ret);
+	ret = led_set_(&led0_, 0);
+	HW_RETURN_IF(ret != 0, ret);
 
-	ret = pwm_set_pulse_dt(&green_pwm_led_, green);
-	BSP_RETURN_IF(ret != 0, ret);
+	ret = led_set_(&led1_, 0);
+	HW_RETURN_IF(ret != 0, ret);
 
-	ret = pwm_set_pulse_dt(&blue_pwm_led_, blue);
-	return ret;
+	return led_set_(&led2_, brightness);
+}
+
+int bsp_led_set_leds(float led0_brightness, float led1_brightness, float led2_brightness)
+{
+	int ret = 0;
+
+	ret = led_set_(&led0_, led0_brightness);
+	HW_RETURN_IF(ret != 0, ret);
+
+	ret = led_set_(&led1_, led1_brightness);
+	HW_RETURN_IF(ret != 0, ret);
+
+	return led_set_(&led2_, led2_brightness);
 }
 
 int bsp_led_off(void)
 {
-	int ret;
+	int ret = 0;
 
-	ret = pwm_set_pulse_dt(&red_pwm_led_, 0);
-	BSP_RETURN_IF(ret != 0, ret);
+	ret = led_set_(&led0_, 0);
+	HW_RETURN_IF(ret != 0, ret);
 
-	ret = pwm_set_pulse_dt(&green_pwm_led_, 0);
-	BSP_RETURN_IF(ret != 0, ret);
+	ret = led_set_(&led1_, 0);
+	HW_RETURN_IF(ret != 0, ret);
 
-	ret = pwm_set_pulse_dt(&blue_pwm_led_, 0);
-	return ret;
+	return led_set_(&led2_, 0);
 }
 
-int bsp_led_blink_red(float brightness, int32_t on_ms, int32_t off_ms)
+int bsp_led_blink_led0(float brightness, int32_t on_ms, int32_t off_ms)
 {
-	int ret;
+	int ret = 0;
 
-	ret = bsp_led_set_red(brightness);
+	ret = bsp_led_set_led0(brightness);
 	BSP_RETURN_IF(ret != 0, ret);
 
 	k_msleep(on_ms);
@@ -133,11 +211,11 @@ int bsp_led_blink_red(float brightness, int32_t on_ms, int32_t off_ms)
 	return ret;
 }
 
-int bsp_led_blink_green(float brightness, int32_t on_ms, int32_t off_ms)
+int bsp_led_blink_led1(float brightness, int32_t on_ms, int32_t off_ms)
 {
 	int ret;
 
-	ret = bsp_led_set_green(brightness);
+	ret = bsp_led_set_led1(brightness);
 	BSP_RETURN_IF(ret != 0, ret);
 
 	k_msleep(on_ms);
@@ -148,11 +226,11 @@ int bsp_led_blink_green(float brightness, int32_t on_ms, int32_t off_ms)
 	return ret;
 }
 
-int bsp_led_blink_blue(float brightness, int32_t on_ms, int32_t off_ms)
+int bsp_led_blink_led2(float brightness, int32_t on_ms, int32_t off_ms)
 {
 	int ret;
 
-	ret = bsp_led_set_blue(brightness);
+	ret = bsp_led_set_led2(brightness);
 	BSP_RETURN_IF(ret != 0, ret);
 
 	k_msleep(on_ms);
@@ -163,11 +241,11 @@ int bsp_led_blink_blue(float brightness, int32_t on_ms, int32_t off_ms)
 	return ret;
 }
 
-int bsp_led_blink_rgb(float r, float g, float b, int32_t on_ms, int32_t off_ms)
+int bsp_led_blink_leds(float led0, float led1, float led2, int32_t on_ms, int32_t off_ms)
 {
 	int ret;
 
-	ret = bsp_led_set_rgb(r, g, b);
+	ret = bsp_led_set_leds(led0, led1, led2);
 
 	BSP_RETURN_IF(ret != 0, ret);
 
--- applications/gesture_recognition/src/bsp/led/bsp_led.h
+++ applications/gesture_recognition/src/bsp/led/bsp_led.h
@@ -16,7 +16,7 @@
 #ifndef __BSP_LED_H__
 #define __BSP_LED_H__
 
-#include <bsp_common.h>
+#include "../bsp_common.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -31,42 +31,42 @@ extern "C" {
 int bsp_led_init(void);
 
 /**
- * @brief Turn on red LED with specific brightness
- * 
+ * @brief Turn on LED channel 0 (red on RGB boards, LED0 on discrete boards)
+ *
  * @param brightness    LED brightness in range 0 - 1
- * 
- * @return Operation status, 0 for success 
+ *
+ * @return Operation status, 0 for success
  */
-int bsp_led_set_red(float brightness);
+int bsp_led_set_led0(float brightness);
 
 /**
- * @brief Turn on green LED with specific brightness
- * 
+ * @brief Turn on LED channel 1 (green on RGB boards, LED1 on discrete boards)
+ *
  * @param brightness    LED brightness in range 0 - 1
- * 
- * @return Operation status, 0 for success 
+ *
+ * @return Operation status, 0 for success
  */
-int bsp_led_set_green(float brightness);
+int bsp_led_set_led1(float brightness);
 
 /**
- * @brief Turn on blue LED with specific brightness
- * 
+ * @brief Turn on LED channel 2 (blue on RGB boards, LED2 on discrete boards)
+ *
  * @param brightness    LED brightness in range 0 - 1
- * 
- * @return Operation status, 0 for success 
+ *
+ * @return Operation status, 0 for success
  */
-int bsp_led_set_blue(float brightness);
+int bsp_led_set_led2(float brightness);
 
 /**
- * @brief Turn on RGB LED with specific brightness for each color
+ * @brief Turn on LED channels 0..2 with specific brightness
  * 
- * @param r    Red LED brightness in range 0 - 1
- * @param g    Green LED brightness in range 0 - 1
- * @param b    Blue LED brightness in range 0 - 1
+ * @param led0_brightness    LED0 (red on RGB boards) brightness in range 0 - 1
+ * @param led1_brightness    LED1 (green on RGB boards) brightness in range 0 - 1
+ * @param led2_brightness    LED2 (blue on RGB boards) brightness in range 0 - 1
  * 
  * @return Operation status, 0 for success 
  */
-int bsp_led_set_rgb(float r, float g, float b);
+int bsp_led_set_leds(float led0_brightness, float led1_brightness, float led2_brightness);
 
 /**
  * @brief Turn off all LEDs
@@ -76,50 +76,58 @@ int bsp_led_set_rgb(float r, float g, float b);
 int bsp_led_off(void);
 
 /**
- * @brief Blink red LED with specific brightness
- * 
+ * @brief Blink LED channel 0 (red on RGB boards, LED0 on discrete boards)
+ *
+ * @note This function uses sleep internally and should not be called from ISR context.
+ *
  * @param brightness  LED brightness in range 0 - 1
  * @param on_ms       LED turns on time in milliseconds
  * @param off_ms      LED turns off time in milliseconds
- * 
+ *
  * @return Operation status, 0 for success
  */
-int bsp_led_blink_red(float brightness, int32_t on_ms, int32_t off_ms);
+int bsp_led_blink_led0(float brightness, int32_t on_ms, int32_t off_ms);
 
 /**
- * @brief Blink green LED with specific brightness
- * 
+ * @brief Blink LED channel 1 (green on RGB boards, LED1 on discrete boards)
+ *
+ * @note This function uses sleep internally and should not be called from ISR context.
+ *
  * @param brightness  LED brightness in range 0 - 1
  * @param on_ms       LED turns on time in milliseconds
  * @param off_ms      LED turns off time in milliseconds
- * 
+ *
  * @return Operation status, 0 for success
  */
-int bsp_led_blink_green(float brightness, int32_t on_ms, int32_t off_ms);
+int bsp_led_blink_led1(float brightness, int32_t on_ms, int32_t off_ms);
 
 /**
- * @brief Blink blue LED with specific brightness
- * 
+ * @brief Blink LED channel 2 (blue on RGB boards, LED2 on discrete boards)
+ *
+ * @note This function uses sleep internally and should not be called from ISR context.
+ *
  * @param brightness  LED brightness in range 0 - 1
  * @param on_ms       LED turns on time in milliseconds
  * @param off_ms      LED turns off time in milliseconds
- * 
+ *
  * @return Operation status, 0 for success
  */
-int bsp_led_blink_blue(float brightness, int32_t on_ms, int32_t off_ms);
+int bsp_led_blink_led2(float brightness, int32_t on_ms, int32_t off_ms);
 
 /**
- * @brief Blink RGB LED with specific brightness for each color
+ * @brief Blink LED channels 0..2 with specific brightness
  * 
- * @param r           Red LED brightness in range 0 - 1
- * @param g           Green LED brightness in range 0 - 1
- * @param b           Blue LED brightness in range 0 - 1
+ * @note This function uses sleep internally and should not be called from ISR context.
+ *
+ * @param led0        LED0 (red on RGB boards) brightness in range 0 - 1
+ * @param led1        LED1 (green on RGB boards) brightness in range 0 - 1
+ * @param led2        LED2 (blue on RGB boards) brightness in range 0 - 1
  * @param on_ms       RGB LED turns on time in milliseconds
  * @param off_ms      RGB LED turns off time in milliseconds
  * 
  * @return Operation status, 0 for success
  */
-int bsp_led_blink_rgb(float r, float g, float b, int32_t on_ms, int32_t off_ms);
+int bsp_led_blink_leds(float led0_brightness, float led1_brightness, float led2_brightness, int32_t on_ms, int32_t off_ms);
 
 #ifdef __cplusplus
 }
