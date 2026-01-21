62ca42e3aca016d94cbb684d0b722ea56eec7235
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2026-01-21 12:00:54 +0100

    applications: gesture_recognition: Simplyfy postprocessing
    Simplify postprocessing by removing callback.
    
    JIRA: NCSDK-36990
    
    Signed-off-by: Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>

--- applications/gesture_recognition/src/inference_postprocessing.c
+++ applications/gesture_recognition/src/inference_postprocessing.c
@@ -6,20 +6,12 @@
 
 #include "inference_postprocessing.h"
 
+#include <stdbool.h>
 #include <string.h>
 #include <zephyr/sys/util.h>
 
 #define PREVIOUS_PREDICTION_NUM                 (3)
 
-
-typedef struct prediction_ctx_s
-{
-	/** Prediction target */
-	uint16_t target;
-
-	/** Prediction probability */
-	float probability;
-} prediction_ctx_t;
 
 typedef struct prediction_tracer_s
 {
@@ -39,16 +31,15 @@ typedef struct prediction_tracer_s
  */
 typedef struct class_prediction_condition_s
 {
-	/** Minimum number of repetitions of a class for prediction */
+	/* Minimum number of repetitions of a class for prediction */
 	uint16_t min_repeat_count;
 
-	/** Minimum probability threshold for prediction */
+	/* Minimum probability threshold for prediction */
 	float probability_threshold;
 } class_prediction_condition_t;
 
 static const char *get_name_by_target_(uint8_t predicted_target)
 {
-
 	static const char * const LABEL_VS_NAME[] = {
 		[CLASS_LABEL_IDLE]           = "IDLE",
 		[CLASS_LABEL_UNKNOWN]        = "UNKNOWN",
@@ -61,6 +52,7 @@ static const char *get_name_by_target_(uint8_t predicted_target)
 	};
 
 	static const uint8_t LABELS_CNT = ARRAY_SIZE(LABEL_VS_NAME);
+	__ASSERT_NO_MSG(LABELS_CNT == CLASS_LABEL_COUNT);
 
 	return (predicted_target < LABELS_CNT) ? LABEL_VS_NAME[predicted_target] : NULL;
 }
@@ -73,7 +65,7 @@ static const class_prediction_condition_t *get_class_condition_(uint8_t predicte
 		[CLASS_LABEL_SWIPE_LEFT]     = {2, 0.8},
 		[CLASS_LABEL_SWIPE_RIGHT]    = {2, 0.8},
 		[CLASS_LABEL_DOUBLE_SHAKE]   = {2, 0.7},
-		[CLASS_LABEL_DOUBLE_THUMB]   = {2, 0.7},
+		[CLASS_LABEL_DOUBLE_THUMB]   = {3, 0.7},
 		[CLASS_LABEL_ROTATION_RIGHT] = {2, 0.7},
 		[CLASS_LABEL_ROTATION_LEFT]  = {2, 0.7},
 	};
@@ -83,72 +75,110 @@ static const class_prediction_condition_t *get_class_condition_(uint8_t predicte
 	return (predicted_target < LABELS_CNT) ? &LABEL_VS_CONFIG[predicted_target] : NULL;
 }
 
-void inference_postprocess(const uint16_t predicted_target,
-							const float prob,
-							const bool do_postprocessing,
-							inference_postprocess_cb_t callback)
+static void reset_tracer_(prediction_tracer_t *tracer, uint16_t target)
 {
-	uint16_t target = predicted_target;
-	float probability = prob;
+	__ASSERT_NO_MSG(tracer != NULL);
+	tracer->index = 0;
+	tracer->target = target;
+}
+
+static void reset_tracer_if_index_overflow_(prediction_tracer_t *tracer)
+{
+	__ASSERT_NO_MSG(tracer != NULL);
+	if (tracer->index >= PREVIOUS_PREDICTION_NUM) {
+		tracer->index = 0;
+	}
+}
+
+static void reset_tracer_if_target_changed_(prediction_tracer_t *tracer, uint16_t target)
+{
+	__ASSERT_NO_MSG(tracer != NULL);
+	if (tracer->target != target) {
+		reset_tracer_(tracer, target);
+	}
+}
+
+static void record_prediction_(prediction_tracer_t *tracer, float probability)
+{
+	__ASSERT_NO_MSG(tracer != NULL);
+	__ASSERT_NO_MSG(tracer->index < PREVIOUS_PREDICTION_NUM);
+	tracer->prev[tracer->index].probability = probability;
+	tracer->index++;
+}
+
+static float average_probability_(const prediction_tracer_t *tracer)
+{
+	float average_prob = 0.0f;
+
+	__ASSERT_NO_MSG(tracer != NULL);
+	__ASSERT_NO_MSG(tracer->index > 0);
+	for (int i = 0; i < tracer->index; ++i) {
+		average_prob += tracer->prev[i].probability;
+	}
+
+	return average_prob / tracer->index;
+}
+
+static bool is_repetitive_class_(uint16_t target)
+{
+	return (target == CLASS_LABEL_ROTATION_RIGHT) ||
+	       (target == CLASS_LABEL_ROTATION_LEFT);
+}
+
+static bool apply_conditions_(const class_prediction_condition_t *condition,
+			      const prediction_tracer_t *tracer,
+			      uint16_t *target,
+			      float *probability)
+{
+	__ASSERT_NO_MSG(condition != NULL);
+	__ASSERT_NO_MSG(tracer != NULL);
+	__ASSERT_NO_MSG(target != NULL);
+	__ASSERT_NO_MSG(probability != NULL);
+	if (tracer->index < condition->min_repeat_count) {
+		*target = CLASS_LABEL_UNKNOWN;
+		*probability = 0.0f;
+		return false;
+	}
+
+	float average_prob = average_probability_(tracer);
+	if (average_prob < condition->probability_threshold) {
+		*target = CLASS_LABEL_UNKNOWN;
+		*probability = 0.0f;
+	} else {
+		*probability = average_prob;
+	}
+
+	return true;
+}
+
+prediction_ctx_t inference_postprocess(uint16_t target, float probability)
+{
+	prediction_ctx_t result = {
+		.target = CLASS_LABEL_UNKNOWN,
+		.probability = 0.0f,
+	};
 	
-	static prediction_tracer_t tracer_ = {0U};
+	static prediction_tracer_t tracer_;
 
-	if (do_postprocessing) {
-		if ((target == CLASS_LABEL_UNKNOWN) || (target == CLASS_LABEL_IDLE)) {
-			/** Reset tracer for UNKNOWN and IDLE classes */
+	if ((target == CLASS_LABEL_UNKNOWN) || (target == CLASS_LABEL_IDLE)) {
+		/* Reset tracer for UNKNOWN and IDLE classes */
+		reset_tracer_(&tracer_, target);
+	} else {
+		reset_tracer_if_index_overflow_(&tracer_);
+		reset_tracer_if_target_changed_(&tracer_, target);
+		record_prediction_(&tracer_, probability);
+
+		const class_prediction_condition_t *class_condition =
+			get_class_condition_(target);
+		if (class_condition == NULL) {
+			return result;
+		}
+
+		bool evaluated = apply_conditions_(class_condition, &tracer_, &target, &probability);
+
+		/* Reset tracer index for non-repetitive classes */
+		if (evaluated && !is_repetitive_class_(target)) {
 			tracer_.index = 0;
-			tracer_.target = target;
-		} else {
-			if (tracer_.index >= PREVIOUS_PREDICTION_NUM) {
-				tracer_.index = 0;
-			}
-
-			/** Reset tracer if predicted class is not the same as previous */
-			if (tracer_.target != target) {
-				tracer_.index = 0;
-				tracer_.target = target;
-			}
-
-			tracer_.prev[tracer_.index].probability = probability;
-			tracer_.index++;
-
-			const class_prediction_condition_t *class_condition =
-				get_class_condition_(target);
-			if (class_condition == NULL) {
-				return;
-			}
-
-			/** Сlass is labled as CLASS_LABEL_UNKNOWN if the number of
-			 * repetitions does not exceed the threshold
-			 */
-			if (tracer_.index >= class_condition->min_repeat_count) {
-				float average_prob = 0.0f;
-				int i;
-
-				/** Calculate average probability for last N
-				 * predictions of the same class
-				 */
-				for (i = 0; i < tracer_.index; ++i) {
-					average_prob += tracer_.prev[i].probability;
-				}
-
-				average_prob = average_prob / tracer_.index;
-
-				/** If average probability is less the class probability threshold,
-				 * the class is labled as CLASS_LABEL_UNKNOWN */
-				if (average_prob < class_condition->probability_threshold) {
-					target = CLASS_LABEL_UNKNOWN;
-				} else {
-					probability = average_prob;
-				}
-
-			/** Reset tracer index for non-repetative classes */
-			if ((target != CLASS_LABEL_ROTATION_RIGHT) &&
-			    (target != CLASS_LABEL_ROTATION_LEFT)) {
-				tracer_.index = 0;
-			}
-		} else {
-			target = CLASS_LABEL_UNKNOWN;
 		}
 	}
 
