afc2d2e3c06c6ea3dbe2172c42387eb549dc3717
Szymon Antkowiak <szymon.antkowiak@nordicsemi.no>
2026-07-10 09:10:15 +0200

    version 2
    models metadata and cmd buffer template are also stored in the model partition, but there is runtime cmd buffer created from template
    
    Signed-off-by: Szymon Antkowiak <szymon.antkowiak@nordicsemi.no>

--- samples/axon/hello_axon/src/model_partition/hello_axon_model_image.h
+++ samples/axon/hello_axon/src/model_partition/hello_axon_model_image.h
@@ -8,18 +8,67 @@
 
 #include <stdint.h>
 
-#define HELLO_AXON_MODEL_IMAGE_MAGIC   0x4E4F5841U /* 'AXON' little-endian */
-#define HELLO_AXON_MODEL_IMAGE_VERSION 1U
-#define HELLO_AXON_MODEL_CONST_SIZE    420U
+#include <zephyr/sys/util.h>
 
-#define NRF_AXON_MODEL_HELLO_AXON_MAX_IL_BUFFER_USED   16
-#define NRF_AXON_MODEL_HELLO_AXON_MAX_PSUM_BUFFER_USED 0
+#define HELLO_AXON_MODEL_IMAGE_MAGIC   0x4E4F5841U /* 'AXON' little-endian */
+#define HELLO_AXON_MODEL_IMAGE_VERSION 2U
+#define HELLO_AXON_MODEL_CONST_SIZE    420U
+#define HELLO_AXON_MODEL_CMD_BUFFER_LEN  69U
+#define HELLO_AXON_MODEL_NAME_MAX_LEN    32U
+
+#define HELLO_AXON_CMD_PTR_INTERLAYER     0xFFFFFFFFU
+#define HELLO_AXON_CMD_PTR_MODEL_CONST(n) (0x80000000U | (uint32_t)(n))
 
 struct hello_axon_model_image_header {
 	uint32_t magic;
 	uint32_t version;
 	uint32_t model_const_size;
-	uint32_t reserved;
+	uint32_t cmd_buffer_len;
+	uint32_t metadata_offset;
+	uint32_t model_const_offset;
+	uint32_t cmd_buffer_offset;
+	uint32_t model_name_offset;
 };
 
-BUILD_ASSERT(sizeof(struct hello_axon_model_image_header) == 16);
+BUILD_ASSERT(sizeof(struct hello_axon_model_image_header) == 32);
+
+struct __packed hello_axon_model_image_metadata {
+	uint32_t compiler_version;
+	uint32_t interlayer_buffer_needed;
+	uint32_t psum_buffer_needed;
+	uint32_t min_driver_version_required;
+	uint32_t output_dequant_mult;
+	uint32_t input_quant_mult;
+	uint16_t input_height;
+	uint16_t input_width;
+	uint16_t input_channel_cnt;
+	uint16_t output_height;
+	uint16_t output_width;
+	uint16_t output_channel_cnt;
+	uint8_t input_byte_width;
+	uint8_t input_quant_round;
+	int8_t input_quant_zp;
+	uint16_t input_stride;
+	uint8_t output_byte_width;
+	uint8_t output_dequant_round;
+	int8_t output_dequant_zp;
+	uint16_t output_stride;
+	uint8_t input_cnt;
+	int8_t external_input_ndx;
+	uint8_t is_external;
+	uint8_t is_layer_model;
+	uint16_t extra_output_cnt;
+};
+
+BUILD_ASSERT(sizeof(struct hello_axon_model_image_metadata) == 52);
+
+struct hello_axon_model_const_layout {
+	int8_t l00_weights[16];
+	int32_t l00_biasp[16];
+	int8_t l01_weights[256];
+	int32_t l01_biasp[16];
+	int8_t l02_weights[16];
+	int32_t l02_biasp[1];
+};
+
+BUILD_ASSERT(sizeof(struct hello_axon_model_const_layout) == HELLO_AXON_MODEL_CONST_SIZE);
--- samples/axon/hello_axon/src/model_partition/model_partition.c
+++ samples/axon/hello_axon/src/model_partition/model_partition.c
@@ -7,8 +7,8 @@
 #include <errno.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
-#include <zephyr/devicetree.h>
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/sys/util.h>
 
@@ -20,82 +20,44 @@
 
 BUILD_ASSERT(PARTITION_EXISTS(axon_model_partition));
 
-struct hello_axon_model_const_layout {
-	int8_t l00_weights[16];
-	int32_t l00_biasp[16];
-	int8_t l01_weights[256];
-	int32_t l01_biasp[16];
-	int8_t l02_weights[16];
-	int32_t l02_biasp[1];
-};
+static NRF_AXON_PLATFORM_BITWIDTH_UNSIGNED_TYPE cmd_buffer_runtime[HELLO_AXON_MODEL_CMD_BUFFER_LEN];
 
-BUILD_ASSERT(sizeof(struct hello_axon_model_const_layout) == HELLO_AXON_MODEL_CONST_SIZE);
-
-struct cmd_buffer_fixup {
-	uint8_t word_index;
-	uint16_t const_offset;
-};
-
-static NRF_AXON_PLATFORM_BITWIDTH_UNSIGNED_TYPE cmd_buffer_runtime[69];
-
-static const uint32_t cmd_buffer_template[69] = {
-	0x1fff0044U,
-	0x02000080U, 0x00010001U, 0U, 0x00330001U,
-	0x02000090U, 0x00100001U, 0U, 0x00330001U,
-	0x050000a0U, 0x00010010U, 0U, 0x00050040U, 0x00010010U, 0U, 0x00030010U,
-	0x000000bcU, 0x00000000U,
-	0x040000c8U, 0x00000000U, 0x00000000U, 0x03010000U, 0x00011703U, 0x00000205U,
-	0x01000180U, 0x00000003U, 0x00000000U,
-	0x010001a4U, 0x00000000U, 0x000116d5U,
-	0x010001c8U, 0xc0000000U, 0xc0000000U,
-	0x000000f0U, 0x00000100U,
-	0x00000080U, 0x00010010U,
-	0x00000088U, 0x00330010U,
-	0x01000090U, 0x00100010U, 0U,
-	0x000000a4U, 0U,
-	0x000001a8U, 0x00035a4eU,
-	0x000000f0U, 0x00000100U,
-	0x01000090U, 0x00010010U, 0U,
-	0x030000a0U, 0x00010001U, 0U, 0x00050004U, 0x00010001U,
-	0x000000b4U, 0x00030001U,
-	0x000000d4U, 0x00011c03U,
-	0x01000180U, 0x00000002U, 0x80000000U,
-	0x000001a8U, 0x00183066U,
-	0x000001ccU, 0x40000000U,
-	0x000000f0U, 0x00000100U,
-};
-
-static const struct cmd_buffer_fixup cmd_buffer_const_fixups[] = {
-	{ .word_index = 7, .const_offset = offsetof(struct hello_axon_model_const_layout, l00_weights) },
-	{ .word_index = 11, .const_offset = offsetof(struct hello_axon_model_const_layout, l00_biasp) },
-	{ .word_index = 41, .const_offset = offsetof(struct hello_axon_model_const_layout, l01_weights) },
-	{ .word_index = 43, .const_offset = offsetof(struct hello_axon_model_const_layout, l01_biasp) },
-	{ .word_index = 50, .const_offset = offsetof(struct hello_axon_model_const_layout, l02_weights) },
-	{ .word_index = 53, .const_offset = offsetof(struct hello_axon_model_const_layout, l02_biasp) },
-};
-
-static const uint8_t cmd_buffer_interlayer_fixups[] = { 3, 14 };
-
-static void build_runtime_cmd_buffer(const struct hello_axon_model_const_layout *model_const)
+static bool header_is_valid(const struct hello_axon_model_image_header *header)
 {
+	return header->magic == HELLO_AXON_MODEL_IMAGE_MAGIC &&
+	       header->version == HELLO_AXON_MODEL_IMAGE_VERSION &&
+	       header->model_const_size == HELLO_AXON_MODEL_CONST_SIZE &&
+	       header->cmd_buffer_len == HELLO_AXON_MODEL_CMD_BUFFER_LEN &&
+	       header->metadata_offset == sizeof(*header) &&
+	       header->model_const_offset ==
+		       header->metadata_offset + sizeof(struct hello_axon_model_image_metadata) &&
+	       header->cmd_buffer_offset == header->model_const_offset + HELLO_AXON_MODEL_CONST_SIZE &&
+	       header->model_name_offset ==
+		       header->cmd_buffer_offset + (HELLO_AXON_MODEL_CMD_BUFFER_LEN * sizeof(uint32_t));
+}
+
+static void build_runtime_cmd_buffer(const uint8_t *partition_base,
+				     const struct hello_axon_model_image_header *header,
+				     const struct hello_axon_model_const_layout *model_const)
+{
+	const uint32_t *cmd_template =
+		(const uint32_t *)(partition_base + header->cmd_buffer_offset);
 	const uintptr_t const_base = (uintptr_t)model_const;
 
-	for (size_t i = 0; i < ARRAY_SIZE(cmd_buffer_template); i++) {
-		cmd_buffer_runtime[i] = cmd_buffer_template[i];
-	}
+	for (size_t i = 0; i < header->cmd_buffer_len; i++) {
+		const uint32_t word = cmd_template[i];
 
-	for (size_t i = 0; i < ARRAY_SIZE(cmd_buffer_interlayer_fixups); i++) {
-		const uint8_t index = cmd_buffer_interlayer_fixups[i];
-
-		cmd_buffer_runtime[index] =
-			(NRF_AXON_PLATFORM_BITWIDTH_UNSIGNED_TYPE)(uintptr_t)nrf_axon_interlayer_buffer;
-	}
-
-	for (size_t i = 0; i < ARRAY_SIZE(cmd_buffer_const_fixups); i++) {
-		const struct cmd_buffer_fixup *fixup = &cmd_buffer_const_fixups[i];
-
-		cmd_buffer_runtime[fixup->word_index] =
-			(NRF_AXON_PLATFORM_BITWIDTH_UNSIGNED_TYPE)(const_base + fixup->const_offset);
+		if (word == HELLO_AXON_CMD_PTR_INTERLAYER) {
+			cmd_buffer_runtime[i] =
+				(NRF_AXON_PLATFORM_BITWIDTH_UNSIGNED_TYPE)(uintptr_t)
+					nrf_axon_interlayer_buffer;
+		} else if ((word & 0xF0000000U) == 0x80000000U) {
+			cmd_buffer_runtime[i] =
+				(NRF_AXON_PLATFORM_BITWIDTH_UNSIGNED_TYPE)(const_base +
+									 (word & 0x0FFFFFFFU));
+		} else {
+			cmd_buffer_runtime[i] = word;
+		}
 	}
 }
 
@@ -104,70 +66,73 @@ int hello_axon_model_partition_load(nrf_axon_nn_compiled_model_s *model)
 	const uint8_t *partition_base = UINT_TO_POINTER(PARTITION_ADDRESS(axon_model_partition));
 	const struct hello_axon_model_image_header *header =
 		(const struct hello_axon_model_image_header *)partition_base;
+	const struct hello_axon_model_image_metadata *metadata;
 	const struct hello_axon_model_const_layout *model_const;
+	const char *model_name;
 
-	if (header->magic != HELLO_AXON_MODEL_IMAGE_MAGIC) {
+	if (!header_is_valid(header)) {
 		return -EINVAL;
 	}
 
-	if (header->version != HELLO_AXON_MODEL_IMAGE_VERSION) {
+	metadata = (const struct hello_axon_model_image_metadata *)(partition_base +
+								     header->metadata_offset);
+	model_const = (const struct hello_axon_model_const_layout *)(partition_base +
+								     header->model_const_offset);
+	model_name = (const char *)(partition_base + header->model_name_offset);
+
+	if (strnlen(model_name, HELLO_AXON_MODEL_NAME_MAX_LEN) >= HELLO_AXON_MODEL_NAME_MAX_LEN) {
 		return -EINVAL;
 	}
 
-	if (header->model_const_size != HELLO_AXON_MODEL_CONST_SIZE) {
-		return -EINVAL;
-	}
-
-	model_const = (const struct hello_axon_model_const_layout *)(partition_base + sizeof(*header));
-	build_runtime_cmd_buffer(model_const);
+	build_runtime_cmd_buffer(partition_base, header, model_const);
 
 	*model = (nrf_axon_nn_compiled_model_s){
-		.compiler_version = 0x00010201,
-		.model_name = "hello_axon",
+		.compiler_version = metadata->compiler_version,
+		.model_name = model_name,
 		.labels = NULL,
 		.inputs = {
 			{
 				.ptr = (int8_t *)nrf_axon_interlayer_buffer,
 				.dimensions = {
-					.height = 1,
-					.width = 1,
-					.channel_cnt = 1,
-					.byte_width = 1,
+					.height = metadata->input_height,
+					.width = metadata->input_width,
+					.channel_cnt = metadata->input_channel_cnt,
+					.byte_width = metadata->input_byte_width,
 				},
-				.quant_mult = 21335090,
-				.stride = 1,
-				.quant_round = 19,
-				.quant_zp = -128,
-				.is_external = true,
+				.quant_mult = metadata->input_quant_mult,
+				.stride = metadata->input_stride,
+				.quant_round = metadata->input_quant_round,
+				.quant_zp = metadata->input_quant_zp,
+				.is_external = metadata->is_external != 0,
 			},
 		},
-		.input_cnt = 1,
-		.external_input_ndx = 0,
+		.input_cnt = metadata->input_cnt,
+		.external_input_ndx = metadata->external_input_ndx,
 		.output_ptr = (int8_t *)nrf_axon_interlayer_buffer,
 		.packed_output_buf = NULL,
-		.interlayer_buffer_needed = NRF_AXON_MODEL_HELLO_AXON_MAX_IL_BUFFER_USED,
-		.psum_buffer_needed = NRF_AXON_MODEL_HELLO_AXON_MAX_PSUM_BUFFER_USED,
+		.interlayer_buffer_needed = metadata->interlayer_buffer_needed,
+		.psum_buffer_needed = metadata->psum_buffer_needed,
 		.cmd_buffer_ptr = cmd_buffer_runtime,
 		.model_const_ptr = model_const,
 		.model_const_size = sizeof(*model_const),
-		.cmd_buffer_len = ARRAY_SIZE(cmd_buffer_runtime),
+		.cmd_buffer_len = header->cmd_buffer_len,
 		.persistent_vars = {
 			.count = 0,
 		},
 		.output_dimensions = {
-			.height = 1,
-			.width = 1,
-			.channel_cnt = 1,
-			.byte_width = 1,
+			.height = metadata->output_height,
+			.width = metadata->output_width,
+			.channel_cnt = metadata->output_channel_cnt,
+			.byte_width = metadata->output_byte_width,
 		},
-		.output_dequant_mult = 4548375,
-		.output_dequant_round = 29,
-		.output_dequant_zp = 4,
-		.output_stride = 4,
-		.is_layer_model = false,
-		.extra_output_cnt = 0,
+		.output_dequant_mult = metadata->output_dequant_mult,
+		.output_dequant_round = metadata->output_dequant_round,
+		.output_dequant_zp = metadata->output_dequant_zp,
+		.output_stride = metadata->output_stride,
+		.is_layer_model = metadata->is_layer_model != 0,
+		.extra_output_cnt = metadata->extra_output_cnt,
 		.extra_outputs = NULL,
-		.min_driver_version_required = 0x00010200,
+		.min_driver_version_required = metadata->min_driver_version_required,
 	};
 
 	return 0;
