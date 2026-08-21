364e9eacf770caa90407b5732e55f97451d1fab3
Szymon Antkowiak <szymon.antkowiak@nordicsemi.no>
2026-08-21 14:04:45 +0200

    samples: align Neuton models neuron buffer definition
    Drop MODEL_OTA_NEUTON_NEURONS_CAP from generated nrf_edgeai_user_model.c
    files; OTA cap-sized scratch now lives in model_ota_neuton_wired.c.in
    
    Signed-off-by: Szymon Antkowiak <szymon.antkowiak@nordicsemi.no>

--- samples/nrf_edgeai/anomaly/src/nrf_edgeai_generated/nrf_edgeai_user_model.c
+++ samples/nrf_edgeai/anomaly/src/nrf_edgeai_generated/nrf_edgeai_user_model.c
@@ -392,12 +392,8 @@ static const nrf_user_output_t MODEL_AVERAGE_EMBEDDING[] = {
 	}
 #endif
 
-#ifndef MODEL_OTA_NEUTON_NEURONS_CAP
-#define MODEL_OTA_NEUTON_NEURONS_CAP MODEL_NEURONS_NUM
-#endif
-
 /** Model neurons activations buffer */
-static nrf_user_neuron_t model_neurons_[MODEL_OTA_NEUTON_NEURONS_CAP];
+static nrf_user_neuron_t model_neurons_[MODEL_NEURONS_NUM];
 
 /** Neuton model instance */
 #ifdef MODEL_OTA_NEUTON_RUNTIME_WIRED
--- samples/nrf_edgeai/classification/src/nrf_edgeai_generated/Neuton/nrf_edgeai_user_model.c
+++ samples/nrf_edgeai/classification/src/nrf_edgeai_generated/Neuton/nrf_edgeai_user_model.c
@@ -390,12 +390,8 @@ static const uint16_t MODEL_OUTPUT_NEURONS_INDICES[] = {71, 67, 77, 11, 75, 79, 
 	}
 #endif
 
-#ifndef MODEL_OTA_NEUTON_NEURONS_CAP
-#define MODEL_OTA_NEUTON_NEURONS_CAP MODEL_NEURONS_NUM
-#endif
-
 /** Model neurons activations buffer */
-static nrf_user_neuron_t model_neurons_[MODEL_OTA_NEUTON_NEURONS_CAP];
+static nrf_user_neuron_t model_neurons_[MODEL_NEURONS_NUM];
 
 /** Neuton model instance */
 #ifdef MODEL_OTA_NEUTON_RUNTIME_WIRED
