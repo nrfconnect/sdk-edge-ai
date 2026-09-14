2ffa7a44fba6a2ee4687108d3a02da6779bf0996
Bartosz Meus <bartosz.meus@nordicsemi.no>
2026-09-14 14:00:37 +0200

    applications: sensor_based_models: update HAR model
    Replace HAR solution 94826 with solution 95877 and adjust
    the input window to match the updated model metadata.
    
    Signed-off-by: Bartosz Meus <bartosz.meus@nordicsemi.no>

--- applications/sensor_based_models/src/models/har/nrf_edgeai_user_model.h
+++ applications/sensor_based_models/src/models/har/nrf_edgeai_user_model.h
@@ -1,12 +1,12 @@
-/* 2026-07-22T11:47:09.978144 */
+/* 2026-09-02T16:05:10.987272 */
 
 /*
 * Copyright (c) 2026 Nordic Semiconductor ASA
 * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 */
 
-#ifndef _NRF_EDGEAI_USER_MODEL_94826_H_
-#define _NRF_EDGEAI_USER_MODEL_94826_H_
+#ifndef _NRF_EDGEAI_USER_MODEL_95877_H_
+#define _NRF_EDGEAI_USER_MODEL_95877_H_
 
 #include <nrf_edgeai/rt/nrf_edgeai_types.h>
 
@@ -17,30 +17,30 @@ extern "C" {
 /** 
  * @brief Get pointer to the Nordic Edge AI Lab model instance (@ref nrf_edgeai_t).
  */
-nrf_edgeai_t* nrf_edgeai_user_model_94826(void);
+nrf_edgeai_t* nrf_edgeai_user_model_95877(void);
 /** 
  * @brief Get size FLASH/ROM size of the Nordic Edge AI model. 
  * 
  * @return Size in bytes of the model.
  */ 
-uint32_t nrf_edgeai_user_model_size_94826(void);
+uint32_t nrf_edgeai_user_model_size_95877(void);
 
 /** 
  * @brief Alias for the Nordic Edge AI Lab user model API name.
  */
 #ifndef nrf_edgeai_user_model
-#define nrf_edgeai_user_model nrf_edgeai_user_model_94826
+#define nrf_edgeai_user_model nrf_edgeai_user_model_95877
 #endif
 
 /** 
  * @brief Alias for the Nordic Edge AI Lab user model size API name.
  */
 #ifndef nrf_edgeai_user_model_size
-#define nrf_edgeai_user_model_size nrf_edgeai_user_model_size_94826
+#define nrf_edgeai_user_model_size nrf_edgeai_user_model_size_95877
 #endif
 
 #ifdef __cplusplus 
 }
 #endif 
 
-#endif /* _NRF_EDGEAI_USER_MODEL_94826_H_ */
+#endif /* _NRF_EDGEAI_USER_MODEL_95877_H_ */
